6fcd187000d7bf86ab4553022f0059b0b4f60065
birolguldal <birolguldal@gmail.com>
2020-05-26 19:37:41 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+//Author: Birol Guldal Email: <gx6265@wayne.edu> 
+//Header file for input output functions 
 #include<iostream>  
   
 using namespace std; 
